33f9ed48f000a12581bb2ef611615df56a39c576
nesterrovv <nesterov.iv2018@gmail.com>
2026-01-14 01:35:47 +0700

    backend fixes

--- casino-mis-application/src/main/java/com/casino/mis/config/AppConfig.java
+++ casino-mis-application/src/main/java/com/casino/mis/config/AppConfig.java
@@ -1,15 +1,19 @@
 package com.casino.mis.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder
+                .modules(new JavaTimeModule())
+                .build();
     }
 }
 
